0e8f8ea56ebd65995c60a066b67821ccc29c4a03
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-26 17:24:13 +0100

    Adds Done button to the entry

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      todos: [{name: "Dishes", priority: "high"},
-      {name: "Washing", priority: "low"},
-      {name: "Feeding the doggos", priority: "high"}],
+      todos: [{name: "Dishes", priority: "high", status: true},
+      {name: "Washing", priority: "low", status: false},
+      {name: "Feeding the doggos", priority: "high", status: false}],
       newTodo: "",
       newPriority: "high"
     },
@@ -15,6 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
           const todoToAdd = {name: this.newTodo, priority: this.newPriority};
           this.todos.push(todoToAdd);
           this.newTodo = "";
+      },
+      doneTodo: function(index) {
+        this.todos[index].status = true;
       }
     }
   });
