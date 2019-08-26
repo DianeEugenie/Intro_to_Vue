9b8b528a376573033a7b4a14442d79622d58cb05
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-26 15:47:25 +0100

    Adds function to add items to the list

--- src/app.js
+++ src/app.js
@@ -4,7 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      todos: ["Dishes", "Washing", "Feeding the doggos"]
+      todos: ["Dishes", "Washing", "Feeding the doggos"],
+      newToDo: ""
+    },
+    methods: {
+      saveNewToDo: function() {
+        if (this.newToDo !== "") {
+          this.todos.push(this.newToDo);
+          this.newToDo = "";
+        };
+      }
     }
   });
 });
+
+
+// methods: {
+//   saveNewItem: function() {
+//     if (this.newItem !== "") {
+//     const itemToAdd = {name: this.newItem, purchased: false};
+//     this.items.push(itemToAdd);
+//     this.newItem = "";
+//     };
+//   },
+//   buyItem: function(index) {
+//     this.items[index].purchased = true;
+//   }
+// }
+// });
+// });
